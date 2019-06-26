48fb4cef8ddf815a166e020d798ee57353fb7f08
Craig A Wiswell <cwiswell63@gmail.com>
2019-06-25 20:54:36 -0400

    Add function button component
    Add function buttons to the calculator component.

--- react-calculator/src/Calculator.tsx
+++ react-calculator/src/Calculator.tsx
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import './Calculator.css';
 
 import NumberButton from  './numberBtnComponent/NumberButton';
+import FunctionButton from './functionBtnComponent/FunctionButton';
 import Result from './resultComponent/Result';
 
 export type CalculatorState = {
@@ -25,15 +26,45 @@ class Calculator extends Component<any, CalculatorState> {
     }
   }
 
+  functionClick = (value: string) => {
+    console.log(value);
+  };
+
   render() {
     console.log("rendering...")
     return (
       <div className="App">
         <Result value={this.state.result}/>
         
-        <NumberButton value={7} click={this.numberClick}/>
-        <NumberButton value={8} click={this.numberClick}/>
-        <NumberButton value={9} click={this.numberClick}/>
+        <FunctionButton value={"CE"} click={this.functionClick} />
+        <FunctionButton value={"C"} click={this.functionClick} />
+        <FunctionButton value={"<"} click={this.functionClick} />
+
+        <FunctionButton value={"÷"} click={this.functionClick} />
+
+        <NumberButton value={7} click={this.numberClick} />
+        <NumberButton value={8} click={this.numberClick} />
+        <NumberButton value={9} click={this.numberClick} />
+
+        <FunctionButton value={"x"} click={this.functionClick} />
+        
+        <NumberButton value={4} click={this.numberClick} />
+        <NumberButton value={5} click={this.numberClick} />
+        <NumberButton value={6} click={this.numberClick} />
+
+        <FunctionButton value={"-"} click={this.functionClick} />
+        
+        <NumberButton value={1} click={this.numberClick} />
+        <NumberButton value={2} click={this.numberClick} />
+        <NumberButton value={3} click={this.numberClick} />
+
+        <FunctionButton value={"+"} click={this.functionClick} />
+
+        <FunctionButton value={"±"} click={this.functionClick} />
+        <NumberButton value={0} click={this.numberClick} />
+        <FunctionButton value={"."} click={this.functionClick} />
+
+        <FunctionButton value={"="} click={this.functionClick} />
       </div>
     );
   }
---
+++ react-calculator/src/functionBtnComponent/FunctionButton.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import './FunctionButton.css';
+
+export type FunctionButtonProp = {
+    value: string;
+    click: Function;
+}
+
+const FunctionButton: React.FC<FunctionButtonProp> = (props) => {
+    return (
+      <div className="FunctionButton" onClick={() => props.click(props.value)}>
+          {props.value}
+      </div>
+    );
+}
+
+export default FunctionButton;
