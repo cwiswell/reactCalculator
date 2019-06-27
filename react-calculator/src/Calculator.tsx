cb3499f76fd38e1fddebc116f4aef86a50b3c930
Craig A Wiswell <cwiswell63@gmail.com>
2019-06-26 21:14:26 -0400

    Add keypress on container div

--- react-calculator/src/Calculator.tsx
+++ react-calculator/src/Calculator.tsx
@@ -1,21 +1,21 @@
 import React, { Component } from 'react';
 import './Calculator.css';
 
-import NumberButton from  './numberBtnComponent/NumberButton';
+import NumberButton from './numberBtnComponent/NumberButton';
 import FunctionButton from './functionBtnComponent/FunctionButton';
 import Result from './resultComponent/Result';
 
 type CalculatorState = {
-   formula: string | null;
-   result: number | null;
-   currentNumber: number | null;
-   previousNumber: number | null;
-   currentOperator: string | null;
+  formula: string | null;
+  result: number | null;
+  currentNumber: number | null;
+  previousNumber: number | null;
+  currentOperator: string | null;
 }
 
 class Calculator extends Component<any, CalculatorState> {
-  
-  state : CalculatorState = { 
+
+  state: CalculatorState = {
     formula: null,
     result: null,
     previousNumber: null,
@@ -23,17 +23,17 @@ class Calculator extends Component<any, CalculatorState> {
     currentOperator: null
   };
 
-  numberClick = (value: number) =>{
-    if(this.state.currentNumber == null){
-      this.setState({currentNumber: value});
-    }else{
-      let number =  `${this.state.currentNumber}${value}`;
-      this.setState({currentNumber: +number});
+  numberClick = (value: number) => {
+    if (this.state.currentNumber == null) {
+      this.setState({ currentNumber: value });
+    } else {
+      let number = `${this.state.currentNumber}${value}`;
+      this.setState({ currentNumber: +number });
     }
   }
 
   functionClick = (value: string) => {
-    switch(value){
+    switch (value) {
       case "<":
         this.backSpace();
         break;
@@ -49,81 +49,104 @@ class Calculator extends Component<any, CalculatorState> {
     }
   };
 
-  arithmetic(value: string){
+  arithmetic(value: string) {
     let newformula = this.addOrUpdateLatestFormulaOperator(value);
-    if(newformula == null)
-    {
+    if (newformula == null) {
       return;
     }
 
-    this.setState({formula: newformula, currentOperator: value, currentNumber: null});
+    this.setState({ formula: newformula, currentOperator: value, currentNumber: null });
   };
 
-  addOrUpdateLatestFormulaOperator(operator: string): string | null{
+  addOrUpdateLatestFormulaOperator(operator: string): string | null {
     let formula = this.state.formula;
     let curNum = this.state.currentNumber;
-    if(formula == null){
+
+    if(formula == null && this.state.currentNumber == null){
+      return null;
+    }
+
+    if (formula == null) {
       formula = `${curNum} ${operator}`;
-    }else if(this.state.currentNumber == null){
-      formula = `${formula.slice(0,-2)} ${operator}`;
-    }else{
+    } else if (this.state.currentNumber == null) {
+      formula = `${formula.slice(0, -2)} ${operator}`;
+    } else {
       formula = `${formula} ${curNum} ${operator}`;
     }
     return formula;
   };
 
-  clearCurrentNumber(){
-    this.setState({currentNumber: 0});
+  clearCurrentNumber() {
+    this.setState({ currentNumber: 0 });
   };
 
-  backSpace(){
-    if(this.state.currentNumber == null){
+  backSpace() {
+    if (this.state.currentNumber == null) {
       return;
     }
     let numberString = `${this.state.currentNumber}`;
-    if(numberString.length < 2){
-      this.setState({currentNumber: null});
+    if (numberString.length < 2) {
+      this.setState({ currentNumber: null });
       return;
     }
 
     numberString = numberString.slice(0, -1);
-    this.setState({currentNumber: +numberString});
+    this.setState({ currentNumber: +numberString });
   };
+
+  handleKeyPress = (event: React.KeyboardEvent<HTMLDivElement>) => {
+    switch (event.key) {
+      case "0":
+      case "1":
+      case "2":
+      case "3":
+      case "4":
+      case "5":
+      case "6":
+      case "7":
+      case "8":
+      case "9":
+        this.numberClick(+event.key);
+    }
+  }
 
   render() {
     return (
-      <div className="App">
-        <Result value={this.state.currentNumber} formula={this.state.formula}/>
-        
-        <FunctionButton value={"CE"} click={this.functionClick} />
-        <FunctionButton value={"C"} click={this.functionClick} />
-        <FunctionButton value={"<"} click={this.functionClick} />
+      <div tabIndex={0} onKeyDown={this.handleKeyPress} className="container">
+        <div className="App">
+          <Result value={this.state.currentNumber} formula={this.state.formula} />
 
-        <FunctionButton value={"÷"} click={this.functionClick} />
+          <FunctionButton value={"CE"} click={this.functionClick} />
+          <FunctionButton value={"C"} click={this.functionClick} />
+          <FunctionButton value={"<"} click={this.functionClick} />
 
-        <NumberButton value={7} click={this.numberClick} />
-        <NumberButton value={8} click={this.numberClick} />
-        <NumberButton value={9} click={this.numberClick} />
+          <FunctionButton value={"÷"} click={this.functionClick} />
 
-        <FunctionButton value={"x"} click={this.functionClick} />
-        
-        <NumberButton value={4} click={this.numberClick} />
-        <NumberButton value={5} click={this.numberClick} />
-        <NumberButton value={6} click={this.numberClick} />
+          <NumberButton value={7} click={this.numberClick} />
+          <NumberButton value={8} click={this.numberClick} />
+          <NumberButton value={9} click={this.numberClick} />
 
-        <FunctionButton value={"-"} click={this.functionClick} />
-        
-        <NumberButton value={1} click={this.numberClick} />
-        <NumberButton value={2} click={this.numberClick} />
-        <NumberButton value={3} click={this.numberClick} />
+          <FunctionButton value={"x"} click={this.functionClick} />
 
-        <FunctionButton value={"+"} click={this.functionClick} />
+          <NumberButton value={4} click={this.numberClick} />
+          <NumberButton value={5} click={this.numberClick} />
+          <NumberButton value={6} click={this.numberClick} />
 
-        <FunctionButton value={"±"} click={this.functionClick} />
-        <NumberButton value={0} click={this.numberClick} />
-        <FunctionButton value={"."} click={this.functionClick} />
+          <FunctionButton value={"-"} click={this.functionClick} />
 
-        <FunctionButton value={"="} click={this.functionClick} />
+          <NumberButton value={1} click={this.numberClick} />
+          <NumberButton value={2} click={this.numberClick} />
+          <NumberButton value={3} click={this.numberClick} />
+
+          <FunctionButton value={"+"} click={this.functionClick} />
+
+          <FunctionButton value={"±"} click={this.functionClick} />
+          <NumberButton value={0} click={this.numberClick} />
+          <FunctionButton value={"."} click={this.functionClick} />
+
+          <FunctionButton value={"="} click={this.functionClick} />
+        </div>
+
       </div>
     );
   }
