1aefd58d8fb16832189aa686522443a9ff0840af
Craig A Wiswell <cwiswell63@gmail.com>
2019-06-28 21:49:48 -0400

    Add unit test for result component

---
+++ react-calculator/src/resultComponent/result.test.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Result from './result';
+
+describe("result component tests", () => {
+
+  it('renders without crashing', () => {
+    const div = document.createElement('div');
+    ReactDOM.render(<Result value={null} formula={null} />, div);
+    ReactDOM.unmountComponentAtNode(div);
+  });
+})
