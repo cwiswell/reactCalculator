5a255c3519bce6dc8957f9a45ae0240439d2d85b
Craig A Wiswell <cwiswell63@gmail.com>
2019-07-02 20:39:44 -0400

    Change result window to use === instead of ==

--- src/resultComponent/result-window.tsx
+++ src/resultComponent/result-window.tsx
@@ -10,10 +10,10 @@ const Result: React.FC<ResultProp> = (props) => {
     return (
       <div className="ResultWindow">
           <div className="FormulaWindow">
-            {props.formula == null ? " " : props.formula}
+            {props.formula === null ? " " : props.formula}
           </div>
           <div className="CurrentNumber">
-            {props.value == null || props.value == "null" ? 0 : props.value}
+            {props.value === null || props.value === "null" ? 0 : props.value}
           </div>          
       </div>
     );
